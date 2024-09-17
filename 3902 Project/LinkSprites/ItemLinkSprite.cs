4fa4f33df593f45f2477a369913624b0ffad4350
Rolina Qu <qu.421@buckeyemail.osu.edu>
2024-09-17 19:18:57 -0400

    Updated SpriteFactory and Link Classes

---
+++ 3902 Project/LinkSprites/ItemLinkSprite.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace _3902_Project
+{
+	public class ItemLinkSprite : ISprite
+	{
+        private Texture2D spritesheet;
+        private int width;
+        private int height;
+        private int frame;
+        private int totalFrames;
+
+
+        public ItemLinkSprite(Texture2D sheet, int w, int h)
+        {
+            //width and height are size of sprite
+            spritesheet = sheet;
+            width = w;
+            height = h;
+            totalFrames = 1;
+            frame = 0;
+        }
+
+        public void Update()
+		{
+            //updates x and y position from LinkMovement.cs
+            frame++;
+            if (frame >= totalFrames)
+            {
+                //frame increments from 0 to count - 1
+                frame = 0;
+            }
+
+        }
+		
+		public void Draw(SpriteBatch sb)
+		{
+			
+
+
+		}
+	}
+
+}
---
+++ 3902 Project/LinkSprites/LinkSpriteFactory.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+using System.ComponentModel;
+//using static _3902_Project.LinkSprite;
+
+namespace _3902_Project
+{
+    public class LinkSpriteFactory
+    {
+        private Texture2D linkSpriteSheet;
+
+        private static LinkSpriteFactory instance = new LinkSpriteFactory();
+
+        private int spriteSize = 16;
+
+        public static LinkSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private LinkSpriteFactory()
+        {
+        }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            linkSpriteSheet = content.Load<Texture2D>("Link Spritesheet");
+        }
+
+        public ISprite CreateWalkingLinkSprite()
+        {
+            //unfortunately opted to initialize the list of textures here
+            //instead of in LinkSprite
+            //List<Texture2D> list = new List<Texture2D>();
+
+            return new WalkingLinkSprite(linkSpriteSheet, spriteSize, spriteSize);
+        }
+
+        public ISprite CreateItemUseLinkSprite()
+        {
+            return new ItemLinkSprite(linkSpriteSheet, spriteSize, spriteSize);
+        }
+
+        public ISprite CreateAttackingLinkSprite()
+        {
+            List<Texture2D> list = new List<Texture2D>();
+            return new AttackingLinkSprite(linkSpriteSheet, spriteSize, spriteSize);
+        }
+        public ISprite CreateDamagedLinkSprite()
+        {
+            List<Texture2D> list = new List<Texture2D>();
+            return new DamagedLinkSprite(linkSpriteSheet, spriteSize, spriteSize);
+        }
+
+
+    }
+}
+
+/*
+// Client code in main game class' LoadContent method:
+LinkSpriteFactory.Instance.LoadAllTextures(Content);
+
+// Client code in Link class:
+ISprite mySprite = EnemySpriteFactory.Instance.CreateBigEnemySprite();
+*/
