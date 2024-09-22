47e81945ea6735c42c17eff9c656e6132a50272c
Rolina Qu <qu.421@buckeyemail.osu.edu>
2024-09-22 14:22:19 -0400

    Added StaionaryLinkSprite method.

--- 3902 Project/Link/LinkSprites/LinkSpriteFactory.cs
+++ 3902 Project/Link/LinkSprites/LinkSpriteFactory.cs
@@ -30,6 +30,16 @@ namespace _3902_Project
             linkSpriteSheet = content.Load<Texture2D>("Link Spritesheet");
         }
 
+        public ISprite StationaryLinkSprite()
+        {
+            List<Rectangle> source = new List<Rectangle>();
+            source.Add(new Rectangle(1, 11, 16, 16));
+            source.Add(new Rectangle(35, 11, 16, 16));
+            source.Add(new Rectangle(69, 11, 16, 16));
+
+            return new LinkSprite(linkSpriteSheet, source, 1, spriteSize, spriteSize);
+        }
+
         public ISprite CreateWalkingLinkSprite()
         {
             //opted to initialize the list of textures here
