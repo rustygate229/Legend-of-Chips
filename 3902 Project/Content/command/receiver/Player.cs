40a27d46b67a18e29e5dbb12aead1a8ea1f8e919
DANwhx <danielwhx1017@gmail.com>
2024-10-07 22:15:30 -0400

    merged

---
+++ 3902 Project/Content/command/receiver/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// CONNECT WITH OTHER LinkPlayer.cs
+namespace _3902_Project
+{
+    public class Player
+    {
+        public void MoveUp()
+        {
+            // Logic for moving player up
+        }
+
+        public void MoveDown()
+        {
+            // Logic for moving player down
+        }
+
+        public void MoveLeft()
+        {
+            // Logic for moving player left
+        }
+
+        public void MoveRight()
+        {
+            // Logic for moving player right
+        }
+
+        public void SwordAttack()
+        {
+            // Logic for player sword attack
+        }
+    }
+}
