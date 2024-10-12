12889a56241d1ad383e2e9b8db960e5cb4503825
rustygate229 <114628290+rustygate229@users.noreply.github.com>
2024-10-12 00:13:51 -0400

    Added interface for environment loading

---
+++ 3902 Project/Sprite Classes/Environment/IEnvironmentFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3902_Project.Sprite_Classes.Environment
+{
+    public interface IEnvironmentFactory
+    {
+
+        void setLevel();
+        int getLevel();
+        void loadLevel();
+        ArraySegment<Rectangle> getCollidables();
+        void Draw();
+        Tuple<int, int> getDimensions();
+    }
+}
