338bb598b6d2d1a7010039a8fbc6707fda04cc9c
Rolina Qu <qu.421@buckeyemail.osu.edu>
2024-10-15 17:50:06 -0400

    time to fix everything woo :D

--- 3902 Project/Content/command/ISprite.cs
+++ 3902 Project/Content/command/ISprite.cs
@@ -6,9 +6,9 @@ public interface ISprite
 
     void Update();
 
-    void Draw(SpriteBatch spriteBatch);
+    //void Draw(SpriteBatch spriteBatch);
 
-    //void Draw(SpriteBatch sb, double x, double y);
+    void Draw(SpriteBatch sb, double x, double y);
 
     //void Draw(SpriteBatch sb, ILinkStateMachine state, double x, double y); womp womp
 }
