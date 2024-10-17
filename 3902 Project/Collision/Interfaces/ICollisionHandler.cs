f193c66b90516d0e56b85bfda75327bb6e04c21b
rustygate229 <114628290+rustygate229@users.noreply.github.com>
2024-10-17 19:45:22 -0400

    Revert "make some change on handler"
    This reverts commit 5ed6083d20af230404432e950c970e6db7dc29a9.

--- 3902 Project/Collision/Interfaces/ICollisionHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace _3902_Project
-{
-    public interface ICollisionHandler
-    {
-        //PlayerCollisionHandler, EnemyCollisionHandler, BlockCollisionHandler, ItemCollisionHandler
-        void HandleCollision(ICollisionBox objectA, ICollisionBox objectB);
-    }
-}
