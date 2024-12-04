22ed609602d5043fef5e674659c202d9ec017c88
rustygate229 <114628290+rustygate229@users.noreply.github.com>
2024-12-04 08:14:18 -0500

    Added backend logic for moving to next room by passing through a door

--- 3902 Project/Environment/EnvironmentFactory.cs
+++ 3902 Project/Environment/EnvironmentFactory.cs
@@ -6,12 +6,12 @@ using System.IO;
 
 namespace _3902_Project
 {
-    public class EnvironmentFactory
+    public partial class EnvironmentFactory
     {
         private BlockManager _blockManager;
         private ItemManager _itemManager;
         private EnemyManager _enemyManager;
-        private CollisionHandlerManager _collisionHandlerManager = new ();
+        private CollisionHandlerManager _collisionHandlerManager = new();
         private ProjectileManager _projectileManager;
         private LinkManager _linkManager;
         private PlaySoundEffect _sound;
@@ -29,10 +29,10 @@ namespace _3902_Project
         private List<List<string>> _enemies;
         private List<List<string>> _items;
 
-        private Vector2 _startingPosition = new (0, 900 - (176 * 4));
+        private Vector2 _startingPosition = new(0, 900 - (176 * 4));
 
         public List<List<ICollisionBox>> _collisionBoxes;
-        
+
 
         public EnvironmentFactory() { }
 
@@ -53,6 +53,8 @@ namespace _3902_Project
             _csvTranslationsEnemy = new Dictionary<string, EnemyManager.EnemyNames>();
             _csvTranslationsItem = new Dictionary<string, ItemManager.ItemNames>();
             generateTranslations();
+
+            loadDoorAssignments();
 
             // Clean up Item Csv
             cleanUpItemCsv();
@@ -162,201 +164,6 @@ namespace _3902_Project
             }
             _blockManager.AddBlock(currentName, new Vector2((int)_startingPosition.X, (int)_startingPosition.Y + 288), 4F);
 
-        }
-
-        private void loadBlocks()
-        {
-            string filepath = Directory.GetCurrentDirectory() + "/../../../Environment/Levels/Level" + _level.ToString() + ".csv";
-            _environment = ReadCsvFile(filepath);
-
-            _blockManager.AddBlock(BlockManager.BlockNames.Environment, new Vector2((int)_startingPosition.X, (int)_startingPosition.Y), printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Wall_DOWN, new Vector2((int)_startingPosition.X + 448, (int)_startingPosition.Y), printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Wall_UP, new Vector2((int)_startingPosition.X + 448, (int)_startingPosition.Y + 576), printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Wall_RIGHT, new Vector2((int)_startingPosition.X, (int)_startingPosition.Y + 288), printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Wall_LEFT, new Vector2((int)_startingPosition.X + 1024 - 128, (int)_startingPosition.Y + 288), printScale);
-            Rectangle TopRight =    new((int)(_startingPosition.X) + (int)(144 * printScale), (int)(_startingPosition.Y), (int)(112 * printScale), (int)(32 * printScale)); //
-            Rectangle TopLeft =     new((int)(_startingPosition.X), (int)(_startingPosition.Y), (int)(112 * printScale), (int)(32 * printScale)); //
-
-            Rectangle BottomRight = new((int)(_startingPosition.X) + (int)(144 * printScale), (int)(_startingPosition.Y) + (int)(144 * printScale), (int)(112 * printScale), (int)(32 * printScale)); //
-            Rectangle BottomLeft =  new((int)(_startingPosition.X), (int)(_startingPosition.Y) + (int)(144 * printScale), (int)(112 * printScale), (int)(32 * printScale)); //
-
-            Rectangle RightBottom = new((int)(_startingPosition.X) + (int)(224 * printScale), (int)(_startingPosition.Y) + (int)(104 * printScale), (int)(32 * printScale), (int)(72 * printScale)); //
-            Rectangle RightTop =    new((int)(_startingPosition.X) + (int)(224 * printScale), (int)(_startingPosition.Y), (int)(32 * printScale), (int)(72 * printScale)); //
-
-            Rectangle LeftBottom =  new((int)(_startingPosition.X), (int)(_startingPosition.Y) + (int)(104 * printScale), (int)(32 * printScale), (int)(112 * printScale)); //
-            Rectangle LeftTop =     new((int)(_startingPosition.X), (int)(_startingPosition.Y), (int)(32 * printScale), (int)(72 * printScale)); //
-            _blockManager.AddBlock(BlockManager.BlockNames.Invisible, TopRight, printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Invisible, TopLeft, printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Invisible, BottomRight, printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Invisible, BottomLeft, printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Invisible, RightBottom, printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Invisible, RightTop, printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Invisible, LeftBottom, printScale);
-            _blockManager.AddBlock(BlockManager.BlockNames.Invisible, LeftTop, printScale);
-
-            for (int i = 0; i < _environment.Count; i++)
-            {
-
-                if (i == 0)
-                    loadDoors(_environment[i]);
-                else
-                {
-                    for (int j = 0; j < _environment[i].Count; j++)
-                    {
-                        string blockToPlace = _environment[i][j];
-
-                        if (_csvTranslationsBlock.ContainsKey(blockToPlace))
-                        {
-                            _blockManager.AddBlock(_csvTranslationsBlock[blockToPlace], new Vector2((int)_startingPosition.X + 128 + (j * 64), (int)_startingPosition.Y + 128 + ((i - 1) * 64)), printScale);
-                        }
-                    }
-                }
-            }
-        }
-
-        private void loadEnemies()
-        {
-            string filepath = Directory.GetCurrentDirectory() + "/../../../Environment/Enemies/Enemy" + _level.ToString() + ".csv";
-            _enemies = ReadCsvFile(filepath);
-
-            for (int i = 0; i < _enemies.Count; i++)
-            {
-                for (int j = 0; j < _enemies[i].Count; j++)
-                {
-                    string enemyToPlace = _enemies[i][j];
-
-                    if (_csvTranslationsEnemy.ContainsKey(enemyToPlace))
-                        _enemyManager.AddEnemy(_csvTranslationsEnemy[enemyToPlace], new Vector2((int)_startingPosition.X + 128 + (j * 64), (int)_startingPosition.Y + 128 + (i * 64)), printScale);
-                }
-            }
-        }
-
-        private void loadItems()
-        {
-            string filepath = Directory.GetCurrentDirectory() + "/../../../Environment/Items/Item" + _level.ToString() + ".csv";
-            _items = ReadCsvFile(filepath);
-
-            for (int i = 0; i < _items.Count; i++)
-            {
-                for (int j = 0; j < _items[i].Count; j++)
-                {
-                    string itemToPlace = _items[i][j];
-
-                    if (!itemToPlace[0].Equals("!") && _csvTranslationsItem.ContainsKey(itemToPlace))
-                    {
-                        _itemManager.AddItem(_csvTranslationsItem[itemToPlace], new Vector2((int)_startingPosition.X + 128 + (j * 64), (int)_startingPosition.Y + 128 + (i * 64)), printScale);
-                    }
-                }
-            }
-        }
-        private void writeToItemCsv(int level)
-        {
-            string filepath = Directory.GetCurrentDirectory() + "/../../../Environment/Items/Item" + level.ToString() + ".csv";
-
-            int rows = _items.Count;
-            int cols = _items[0].Count;
-
-            using (StreamWriter writer = new StreamWriter(filepath))
-            {
-                for (int i = 0; i < rows; i++)
-                {
-                    string[] rowValues = new string[cols];
-                    for (int j = 0; j < cols; j++)
-                    {
-                        rowValues[j] = _items[i][j];
-                    }
-
-                    // Write the row as a comma-separated line
-                    writer.WriteLine(string.Join(",", rowValues));
-                }
-            }
-        }
-        public void deloadItem(ISprite sprite)
-        {
-            int x = (int)sprite.PositionOnWindow.X;
-            int y = (int)sprite.PositionOnWindow.Y;
-
-            // translate position to indeces
-            int row = (y - 128 - (int)_startingPosition.Y) / 64;
-            int col = (x - 128 - (int)_startingPosition.X) / 64;
-
-            _items[row][col] = "!" + _items[row][col];
-            writeToItemCsv(_level);
-        }
-
-        private void cleanUpItemCsv()
-        {
-            for (int level = _level; level < _endLevel + 1; level++)
-            {
-                string filepath = Directory.GetCurrentDirectory() + "/../../../Environment/Items/Item" + level.ToString() + ".csv";
-                _items = ReadCsvFile(filepath);
-
-                for (int i = 0; i < _items.Count; i++)
-                {
-                    for (int j = 0; j < _items[i].Count; j++)
-                    {
-                        if (_items[i][j][0].Equals('!'))
-                        {
-                            _items[i][j] = _items[i][j].Substring(1);
-                        }
-                    }
-                }
-                writeToItemCsv(level);
-                _items.Clear();
-            }
-        }
- 
-        // expects this to be called AFTER everything else has loaded so collision boxes can be correctly added 
-        public void loadCollisions()
-        {
-            _collisionBoxes.Clear();
-
-            // add the collision boxes IN ORDER (VERY IMPORTANT)
-            _collisionBoxes.Add(_linkManager.GetCollisionBoxes());
-            _collisionBoxes.Add(_enemyManager.GetCollisionBoxes());
-            _collisionBoxes.Add(_blockManager.GetCollisionBoxes());
-            _collisionBoxes.Add(_projectileManager.GetCollisionBoxes());
-            _collisionBoxes.Add(_itemManager.GetCollisionBoxes());
-        }
-
-        public void loadLevel()
-        {
-            // _linkManager.LinkPositionOnWindow = new (_startingPosition.X + 140, _startingPosition.Y + 310);
-            loadBlocks();
-            loadEnemies();
-            loadItems();
-            loadCollisions();
-        }
-
-        public void incrementLevel()
-        {
-            if (_level < _endLevel) { _level++; }
-        }
-
-        public void decrementLevel()
-        {
-            if (_level > 1) { _level--; }
-        }
-
-        public void Update()
-        {
-            if (_prevLevel != -1 && _prevLevel != _level)
-            {
-                _enemyManager.UnloadAllEnemies();
-                _itemManager.UnloadAllItems();
-                _blockManager.UnloadAllBlocks();
-                _projectileManager.UnloadAllProjectiles();
-
-                loadLevel();
-            }
-
-            _prevLevel = _level;
-
-            // Detect Collisions
-            List<CollisionData> detectedCollisions = CollisionDetector.DetectCollisions(_collisionBoxes);
-
-            // Handle Collisions
-            _collisionHandlerManager.HandleCollisions(detectedCollisions);
         }
     }
 }
---
+++ 3902 Project/Environment/EnvironmentFactoryPartial2.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static _3902_Project.BlockManager;
+
+namespace _3902_Project
+{
+    public partial class EnvironmentFactory
+    {
+        Dictionary<int, Dictionary<BlockNames, int>> openDoorAssignments = new Dictionary<int, Dictionary<BlockNames, int>>();
+
+        private void loadDoorAssignments()
+        {
+
+            // Level 1
+            Dictionary<BlockNames, int> level1 = new Dictionary<BlockNames, int>();
+            level1.Add(BlockNames.OpenDoor_LEFT, 2);
+            openDoorAssignments.Add(1, level1);
+
+            // Level 2
+            Dictionary<BlockNames, int> level2 = new Dictionary<BlockNames, int>();
+            level2.Add(BlockNames.OpenDoor_RIGHT, 1);
+            level2.Add(BlockNames.OpenDoor_LEFT, 3);
+            openDoorAssignments.Add(1, level2);
+
+            // Level 3
+            Dictionary<BlockNames, int> level3 = new Dictionary<BlockNames, int>();
+            level3.Add(BlockNames.OpenDoor_RIGHT, 2);
+            level3.Add(BlockNames.OpenDoor_DOWN, 4);
+            level3.Add(BlockNames.OpenDoor_UP, 5);
+            openDoorAssignments.Add(1, level3);
+
+            // Level 4
+            Dictionary<BlockNames, int> level4 = new Dictionary<BlockNames, int>();
+            level4.Add(BlockNames.OpenDoor_UP, 3);
+            openDoorAssignments.Add(1, level4);
+
+            // Level 5
+            Dictionary<BlockNames, int> level5 = new Dictionary<BlockNames, int>();
+            level5.Add(BlockNames.OpenDoor_DOWN, 3);
+            openDoorAssignments.Add(1, level5);
+        }
+
+        private void moveToNextRoom(BlockNames name)
+        {
+            _level = openDoorAssignments[_level][name];
+        }
+    }
+        
+}
